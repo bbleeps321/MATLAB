36cc16e646c7b72e4401e8a6768f57f5b60a0b57
Carey Zhang <carey.zhang@gmail.com>
2015-02-02 19:40:59 -0800

    Small fix

--- cs155/hw2/qE.m
+++ cs155/hw2/qE.m
@@ -11,7 +11,7 @@ mean(ypredHMM==data.mood')
 %% Train on first 4/5, test on final 5th.
 partIdx = floor(4/5*length(data.mood));
 [A,O] = trainHMM(data.mood(1:partIdx),data.genre(1:partIdx))
-ypredHMMIn = viterbiDP(data.genre(1:partIdx)-1,A,O);
+ypredHMMIn = viterbiDP(data.genre(1:partIdx)-1,A,O)
 mean(ypredHMMIn == data.mood(1:partIdx)')
 ypredHMMOut = viterbiDP(data.genre(partIdx+1:end)-1,A,O);
 mean(ypredHMMOut == data.mood(partIdx+1:end)')
